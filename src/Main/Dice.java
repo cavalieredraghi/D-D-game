117c1fc1c1d28eba6ab79b298fc5f85f665dc4ae
Anthony Holt <Anthony Holt>
2016-10-05 15:43:15 -0400

    working on some dice intergration, so far tests are good.

--- src/Main/Dice.java
+++ src/Main/Dice.java
@@ -12,8 +12,26 @@ public class Dice {
 	public void getd12(){
 		System.out.println(ThreadLocalRandom.current().nextInt(1, 12 + 1));
 	}
+//This one is for a percentile or 100 sided die
+	public void getd100(){
+		System.out.println(ThreadLocalRandom.current().nextInt(1, 100 + 1));
+	}
+//This one is for a 10 sided die
+	public void getd10(){
+		System.out.println(ThreadLocalRandom.current().nextInt(1, 10 + 1));
+	}
 	
-	
-	
+	//this one is for a 8 sided die
+	public void getd8(){
+		System.out.println(ThreadLocalRandom.current().nextInt(1, 8 + 1));
+	}
+	//this one is for a 6 sided die
+	public void getd6(){
+		System.out.println(ThreadLocalRandom.current().nextInt(1, 6 + 1));
+	}
+	//this is for a 4 sided die
+	public void getd4(){
+		System.out.println(ThreadLocalRandom.current().nextInt(1, 4 + 1));
+	}
 	
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,27 +7,30 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Dice d20 = new Dice();
-		
-		d20.getd20();
-		
-		Scanner inputInfo = new Scanner (System.in);
 
-		
+		// Dice sets are made here
+		Dice d = new Dice();
+		int die = 0;
+
+		Scanner inputInfo = new Scanner(System.in);
+
 		System.out.println("Welcome to Drixtel's Might(beta):");
-		System.out.println("In the is (beta) Create Your Own Adventure, you will have not have as many options avaible to you per the usual type of game.");
-		System.out.println("Note that at the end of a description in the story. You will be presented with choices make sure to correctly type the choices presented. ");
-		System.out.println("Do you understand? Yes/No:");
+		System.out.println(
+				"In the is (beta) Create Your Own Adventure, you will have not have as many options avaible to you per the usual type of game.");
+		System.out.println(
+				"Note that at the end of a description in the story. You will be presented with choices make sure to correctly type the choices presented. ");
+		System.out.println("Do you understand? (y)es/(n)o:");
 		String str = inputInfo.nextLine();
-			switch (str){
-				case "Yes" : str = "Good you are ready to go!";
-					break;
-				case "No": str = "Nice Try!";
-					break;
-			}
-			System.out.println(str);
-			
-			
+		switch (str) {
+		case "y":
+			str = "Good you are ready to go!";
+			break;
+		case "n":
+			str = "Nice Try!";
+			break;
+		}
+		System.out.println(str);
+
 		System.out.println("Now to set the stage:");
 		System.out.println("Your journey has taken you far and wide. Your experience had given you");
 		System.out.println("a name in the land, here and possibly beyond. That is what you think at");
@@ -39,17 +42,55 @@ public class Main {
 		System.out.println("Your journey West takes several days as the air grows drier and drier. ");
 		System.out.println("Till eventually you come across a ravine that looks like a keen place for ");
 		System.out.println("an ambush.");
-		
-		System.out.println("Do you go down the ravine or try and find a way around: Ravine/Explore");
+
+		System.out.println("Do you go down the ravine or try and find a way around: (r)avine/(e)xplore");
 		String fc = inputInfo.nextLine();
-			switch(fc){
-				case "Ravine" : fc = "You venture down the Ravine.";
-					break;
-				case "Explore" : fc = "You explore for the next few hours around the ravine and ways to navigate some craig like rock out crops till you conclude you must travel down the Ravine.";
-					break;
-						}
+		switch (fc) {
+		case "r":
+			fc = "You venture down the Ravine.";
+			break;
+		case "e":
+			fc = "You explore for the next few hours around the ravine and ways to navigate some craig like rock out crops till you conclude you must travel down the Ravine.";
+			break;
+		}
 		System.out.println(fc);
+		System.out.println("Thank you for playing that initial version of the Drixtel's Might game.");
+		System.out.println(" feel free to play around now with a dice roller program, which will hopfully be implemented later into the game.");
 		
+		while (die != -1) {
+			System.out.println("100) for 100 sided die");
+			System.out.println("20) for 20 sided die");
+			System.out.println("12) for 12 sided die");
+			System.out.println("10) for 10 sided die");
+			System.out.println("8) for 8 sided die");
+			System.out.println("6) for 6 sided die");
+			System.out.println("4) for 4 sided die");
+			System.out.println("-1) to escape");
+
+			die = inputInfo.nextInt();
+			if (die == 100) {
+				d.getd100();
+			}
+			if (die == 20){
+				d.getd20();
+			}
+			if (die == 12){
+				d.getd12();
+			}
+			if (die == 10){
+				d.getd10();
+			}
+			if (die == 8){
+				d.getd8();
+			}
+			if (die == 6){
+				d.getd6();
+			}
+			if (die == 4){
+				d.getd4();
+			}
+		}
+
 	}
 
 }
