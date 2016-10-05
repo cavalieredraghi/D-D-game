1649900704aed49284ba89349ac15fce1b7712e2
Anthony Holt <Anthony Holt>
2016-10-05 16:49:05 -0400

    added a few new classes for the future game.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -11,14 +11,17 @@ public class Main {
 		// Dice sets are made here
 		Dice d = new Dice();
 		int die = 0;
-
+		// to skip lines while in a Syso This is to keep the story elements better together
+		String newLine = System.getProperty("line.separator");
+		
 		Scanner inputInfo = new Scanner(System.in);
 
-		System.out.println("Welcome to Drixtel's Might(beta):");
-		System.out.println(
-				"In the is (beta) Create Your Own Adventure, you will have not have as many options avaible to you per the usual type of game.");
-		System.out.println(
-				"Note that at the end of a description in the story. You will be presented with choices make sure to correctly type the choices presented. ");
+		System.out.println("Welcome to Drixtel's Might(beta):"+newLine + 
+				"In the is (beta) Create Your Own Adventure," + newLine + 
+				" you will have not have as many options avaible to you per the usual this type of game."+ newLine +
+				"Note that at the end of a description in the story." + newLine+ 
+				" You will be presented with choices make sure to correctly type the choices presented." +newLine +
+				"Your choices will be in () simply type that character to progress.");
 		System.out.println("Do you understand? (y)es/(n)o:");
 		String str = inputInfo.nextLine();
 		switch (str) {
@@ -32,16 +35,16 @@ public class Main {
 		System.out.println(str);
 
 		System.out.println("Now to set the stage:");
-		System.out.println("Your journey has taken you far and wide. Your experience had given you");
-		System.out.println("a name in the land, here and possibly beyond. That is what you think at");
-		System.out.println("least when you receive a letter from a well known Lord Drixtel. He has");
-		System.out.println("called for you personally to aid him in a possible job. His letter ");
-		System.out.println("explains that he wished to see you. It was properly sealed so it had to");
-		System.out.println("genuine. You then set out toward the Desert City-State of Conil, the ");
-		System.out.println("jewel of the Azure Desert. You travel from the city known as Crossroads.");
-		System.out.println("Your journey West takes several days as the air grows drier and drier. ");
-		System.out.println("Till eventually you come across a ravine that looks like a keen place for ");
-		System.out.println("an ambush.");
+		System.out.println("Your journey has taken you far and wide. Your experience had given you"+newLine+
+							"a name in the land, here and possibly beyond. That is what you think at"+newLine+
+							"least when you receive a letter from a well known Lord Drixtel. He has"+newLine+
+							"called for you personally to aid him in a possible job. His letter "+newLine+
+							"explains that he wished to see you. It was properly sealed so it had to"+newLine+
+							"genuine. You then set out toward the Desert City-State of Conil, the "+newLine+
+							"jewel of the Azure Desert. You travel from the city known as Crossroads."+newLine+
+							"Your journey West takes several days as the air grows drier and drier. "+newLine+
+							"Till eventually you come across a ravine that looks like a keen place for "+newLine+
+							"an ambush.");
 
 		System.out.println("Do you go down the ravine or try and find a way around: (r)avine/(e)xplore");
 		String fc = inputInfo.nextLine();
@@ -53,6 +56,7 @@ public class Main {
 			fc = "You explore for the next few hours around the ravine and ways to navigate some craig like rock out crops till you conclude you must travel down the Ravine.";
 			break;
 		}
+		
 		System.out.println(fc);
 		System.out.println("Thank you for playing that initial version of the Drixtel's Might game.");
 		System.out.println(" feel free to play around now with a dice roller program, which will hopfully be implemented later into the game.");
@@ -93,8 +97,11 @@ public class Main {
 			}else{
 				System.out. println ( "Error please try again.");
 			}
-		}
+		}// end of dice simulator
 
 	}
 
 }
+
+
+
